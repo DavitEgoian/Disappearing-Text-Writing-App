16361b907808bb0cccd1767cd582ec5275eb7ad1
DavitEgoian <dato.egoyan@gmail.com>
2025-04-29 00:22:35 +0400

    Main py mainloop has been added

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import tkinter as tk
+from writing_app import DisappearingTextWritingApp
 
+def main():
+    root = tk.Tk()
+    app = DisappearingTextWritingApp(root, timeout_seconds=5)
+    root.mainloop()
 
+if __name__ == "__main__":
+    main()
