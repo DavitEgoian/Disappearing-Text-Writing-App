90b2c7a36c9bb460528446fd24a3a27e53d5cc65
DavitEgoian <dato.egoyan@gmail.com>
2025-04-29 00:12:37 +0400

    Countdown reset timeout and cancel has been added

--- countdown_timer.py
+++ countdown_timer.py
@@ -1,8 +1,21 @@
 import tkinter as tk
 class CountdownTimer:
-
     def __init__(self, root: tk.Misc, timeout_ms: int, on_timeout):
         self.root = root
         self.timeout_ms = timeout_ms
         self.on_timeout = on_timeout
         self._job = None
+
+    def reset(self):
+        if self._job:
+            self.root.after_cancel(self._job)
+        self._job = self.root.after(self.timeout_ms, self._timeout)
+
+    def timeout(self):
+        self._job = None
+        self.on_timeout()
+
+    def cancel(self):
+        if self._job:
+            self.root.after_cancel(self._job)
+            self._job = None
